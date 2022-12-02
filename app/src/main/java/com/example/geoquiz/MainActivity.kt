c929119ba6fbf650ce3983ca0b9c231b56fbf0f1
Richard <85865743+Richard-Van-Intania@users.noreply.github.com>
2022-12-02 23:07:12 +0700

    fifth commit

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -9,85 +9,108 @@ import com.google.android.material.snackbar.Snackbar
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    //  private lateinit var textQuestion: TextView
-    //  private lateinit var buttonTrue: Button
-    //  private lateinit var buttonFalse: Button
-    //  private lateinit var buttonNext: Button
-    //  private lateinit var buttonDebug: Button
+  //  private lateinit var textQuestion: TextView
+  //  private lateinit var buttonTrue: Button
+  //  private lateinit var buttonFalse: Button
+  //  private lateinit var buttonNext: Button
+  //  private lateinit var buttonDebug: Button
 
-    private lateinit var binding: ActivityMainBinding
+  private lateinit var binding: ActivityMainBinding
 
-    private val questionBank =
-        listOf(
-            Question(R.string.question_australia, true),
-            Question(R.string.question_oceans, true),
-            Question(R.string.question_mideast, false),
-            Question(R.string.question_africa, false),
-            Question(R.string.question_americas, true),
-            Question(R.string.question_asia, true)
-        )
+  private val questionBank =
+      listOf(
+          Question(R.string.question_australia, true),
+          Question(R.string.question_oceans, true),
+          Question(R.string.question_mideast, false),
+          Question(R.string.question_africa, false),
+          Question(R.string.question_americas, true),
+          Question(R.string.question_asia, true))
 
-    private var currentIndex = 0
+  private var currentIndex = 0
+  private var correctCount = 0
+  private var incorrectCount = 0
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    //    textQuestion = findViewById(R.id.textQuestion)
+    //    buttonTrue = findViewById(R.id.buttonTrue)
+    //    buttonFalse = findViewById(R.id.buttonFalse)
+    //    buttonNext = findViewById(R.id.buttonNext)
+    //    buttonDebug = findViewById(R.id.buttonDebug)
 
-        //    textQuestion = findViewById(R.id.textQuestion)
-        //    buttonTrue = findViewById(R.id.buttonTrue)
-        //    buttonFalse = findViewById(R.id.buttonFalse)
-        //    buttonNext = findViewById(R.id.buttonNext)
-        //    buttonDebug = findViewById(R.id.buttonDebug)
+    //    buttonTrue.setOnClickListener { view: View ->
+    //      Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+    //    }
+    //    buttonFalse.setOnClickListener { view: View ->
+    //      Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+    //    }
+    //
+    //    buttonNext.setOnClickListener { view: View -> }
+    //
+    //    buttonDebug.setOnClickListener { view: View ->
+    //      Snackbar.make(view, "Debug message", Snackbar.LENGTH_INDEFINITE)
+    //          .setAction(R.string.ok, {})
+    //          .show()
+    //    }
 
-        //    buttonTrue.setOnClickListener { view: View ->
-        //      Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
-        //    }
-        //    buttonFalse.setOnClickListener { view: View ->
-        //      Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
-        //    }
-        //
-        //    buttonNext.setOnClickListener { view: View -> }
-        //
-        //    buttonDebug.setOnClickListener { view: View ->
-        //      Snackbar.make(view, "Debug message", Snackbar.LENGTH_INDEFINITE)
-        //          .setAction(R.string.ok, {})
-        //          .show()
-        //    }
-
-        binding.buttonTrue.setOnClickListener { view: View ->
-            Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT)
-                .setAction(R.string.ok) {}
-                .show()
-        }
-
-        binding.buttonFalse.setOnClickListener { view: View ->
-            Snackbar.make(view, R.string.incorrect_toast, Snackbar.LENGTH_SHORT)
-                .setAction(R.string.ok) {}
-                .show()
-        }
-
-        binding.buttonNext.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateText()
-        }
-
-        binding.buttonRandom.setOnClickListener {
-            currentIndex = Random.nextInt(questionBank.size)
-            updateText()
-        }
-
-        binding.buttonDebug.setOnClickListener { view: View ->
-            Toast.makeText(this, "this is a debug jaaa", Toast.LENGTH_SHORT).show()
-        }
-
+    binding.buttonTrue.setOnClickListener { view: View ->
+      if (questionBank[currentIndex].answer) {
+        Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT)
+            .setAction(R.string.ok) {}
+            .show()
+        correctCount++
+        currentIndex = (currentIndex + 1) % questionBank.size
         updateText()
+      } else {
+        Snackbar.make(view, R.string.incorrect_toast, Snackbar.LENGTH_SHORT)
+            .setAction(R.string.ok) {}
+            .show()
+        incorrectCount++
+        currentIndex = (currentIndex + 1) % questionBank.size
+        updateText()
+      }
     }
 
-
-    private fun updateText() {
-        binding.textQuestion.setText(questionBank[currentIndex].textResId)
+    binding.buttonFalse.setOnClickListener { view: View ->
+      if (!questionBank[currentIndex].answer) {
+        Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT)
+            .setAction(R.string.ok) {}
+            .show()
+        correctCount++
+        currentIndex = (currentIndex + 1) % questionBank.size
+        updateText()
+      } else {
+        Snackbar.make(view, R.string.incorrect_toast, Snackbar.LENGTH_SHORT)
+            .setAction(R.string.ok) {}
+            .show()
+        incorrectCount++
+        currentIndex = (currentIndex + 1) % questionBank.size
+        updateText()
+      }
     }
+
+    binding.buttonNext.setOnClickListener {
+      currentIndex = (currentIndex + 1) % questionBank.size
+      updateText()
+    }
+
+    binding.buttonRandom.setOnClickListener {
+      currentIndex = Random.nextInt(questionBank.size)
+      updateText()
+    }
+
+    binding.buttonDebug.setOnClickListener {
+      Toast.makeText(this, "${correctCount}/${incorrectCount}", Toast.LENGTH_SHORT).show()
+    }
+
+    updateText()
+  }
+
+  private fun updateText() {
+    binding.textQuestion.setText(questionBank[currentIndex].textResId)
+  }
 }
